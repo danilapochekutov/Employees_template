68f633e62ba07668d5124672344a101fdfde0ee9
danilapochekutov <danya270203@gamil.com>
2024-01-26 21:21:37 +0300

    add dev fils

---
+++ src/components/employees-list/employees-list.js
@@ -0,0 +1,21 @@
+import EmployeesListItem from "../employees-list-item/employees-list-item";
+import "./employees-list.css";
+
+const EmployeesList = ({data, onDelete, onToggleProp, onToggleSalary}) => {
+	const elements = data.map((item) => {
+		const {id, ...itemProps} = item;
+		return (
+			<EmployeesListItem
+				key={id}
+				{...itemProps}
+				onDelete={() => onDelete(id)}
+				onToggleProp={(e) => onToggleProp(id, e.currentTarget.getAttribute("data-toggle"))}
+				onToggleSalary={(e) => onToggleSalary(id, e.target.value)}
+			/>
+		);
+	});
+
+	return <ul className='app-list list-group'>{elements}</ul>;
+};
+
+export default EmployeesList;
